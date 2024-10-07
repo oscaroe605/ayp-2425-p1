8ea405301c3a1fbc62f09a4e2f086d16a8f043d0
oscaroe605 <168094860+oscaroe605@users.noreply.github.com>
2024-10-07 08:32:13 -0400

    Update ejercicio-3.py

--- ejercicio-3.py
+++ ejercicio-3.py
@@ -1,10 +1,34 @@
-def is_path_crossing(path: str) -> bool:
-  pass
-
-def main():
-
-  assert is_path_crossing("NES") == False
-  print('1-pass')
-  assert is_path_crossing("NESWW") == True
-  print('2-pass')
-  print('ok')
+direcciones = input("indique la direccion en la que se desea mover: ")
+igual_x = 0
+igual_y = 0
+y = 0
+lista_y = [0]
+x = 0
+lista_x = [0]
+for i in direcciones:
+    if i == "N" or i == "n":
+        y += 1
+        lista_y.append (y)
+    elif i == "S" or i == "s":
+        y -= 1
+        lista_y.append (y)
+    elif i == "E" or i == "e":
+        x += 1
+        lista_x.append (x)
+    elif i == "W" or i == "w":
+        x -= 1
+        lista_x.append (x)
+    else:
+        print ("Por favor introduzca una direccion aceptable") 
+for j in lista_x:
+    for k in lista_x:
+        if j == k:
+            igual_x += 1
+for l in lista_y:
+    for m in lista_y:
+        if l == m:
+            igual_y += 1
+if igual_x > len(direcciones) and igual_y > len(direcciones) :
+    print("True")
+else:
+    print("False")
